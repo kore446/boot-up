2aeeca5306f9a559fdb8ea7a8cb75a4186086288
kore446 <mithul.tls8@gmail.com>
2026-03-24 16:46:03 +0530

    Delete boot-up directory

--- boot-up/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BootUp from "./BootUp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BootUp />
-  </React.StrictMode>
-);
